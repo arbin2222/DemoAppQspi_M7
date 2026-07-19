bb57044a8049b9cffa7f0dec4eb5f35a4da137af
arbin2222 <arbintaj125@gmail.com>
2026-07-19 11:05:10 +0530

    Updated code

--- generic_utilities/read_excel.py
+++ generic_utilities/read_excel.py
@@ -1,10 +1,20 @@
+import os
 from xlrd import *
 
 def read():
-    wb=open_workbook("C:\\Users\\Levono\\PycharmProjects\\framework_DemoAppQspiders(M7)\\generic_utilities\\excel.xlsx")
+    a=os.path.join(
+        os.getcwd(),
+        "generic_utilities",
+        "excel.xlsx"
+    )
+    wb=open_workbook(a)
     sheet=wb.sheet_by_name("Sheet1")
     row=sheet.row_values(1)               #['arbin','arbin@gmail.com',8687678]
     username=row[0]
     email=row[1]
     pwd=row[2]
     return username,email,pwd
+
+
+
+
